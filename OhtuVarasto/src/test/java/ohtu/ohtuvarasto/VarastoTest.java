e9b832239b3ce4ea7f519b31d2026c1e2ff4149f
Vuorela Joonatan A <vujo@da5-cs-bk107-15.helsinki.fi>
2015-09-03 12:04:24 +0300

    broke again

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -72,7 +72,7 @@ public class VarastoTest {
 
         double saatuMaara = varasto.otaVarastosta(2);
        //2
-        assertEquals(7, saatuMaara, vertailuTarkkuus);
+        assertEquals(8, saatuMaara, vertailuTarkkuus);
     }
 
     @Test
